0282e3ecaa3a51752255a5e1498ff268ac5d6c06
Sr_Stroncio <razukmamo@gmail.com>
2025-02-26 20:08:13 +0000

    filter de asociaciones

--- src/pages/Asociaciones.jsx
+++ src/pages/Asociaciones.jsx
@@ -2,18 +2,26 @@ import "../css/Asociaciones.css";
 import { Asociacion } from "../components/Asociacion";
 import VentanaDerecha from "../components/VentanaDerecha";
 import useLlamadaApi from "../providers/useLlamadaApi";
+import { useEstetico } from '../providers/ProviderEstetico';
 
 export function Asociaciones() {
     const { datosApi, loading } = useLlamadaApi('associations');
+    const { searchQuery } = useEstetico();
 
     if (loading) {
         return <div>Loading...</div>;
     }
 
+        // Filtrar eventos según el searchQuery 
+        const eventosFiltrados = datosApi.data.filter(evento =>
+            evento.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        
+
     return (
         <div className="_asociaciones_main_container">
             <div className="_asociaciones_list">
-                {datosApi.data.map((asociacion) => (
+                {eventosFiltrados.data.map((asociacion) => (
                     <Asociacion 
                         key={asociacion.id} 
                         id={asociacion.id}
