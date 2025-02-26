c7222e3e165db97ba5bc9d4bb2d5ce3bd6727681
Sr_Stroncio <razukmamo@gmail.com>
2025-02-26 12:03:20 +0000

    filter

--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -2,38 +2,40 @@ import "../css/Eventos.css";
 import { Evento } from "../components/Evento";
 import VentanaDerecha from "../components/VentanaDerecha";
 import useLlamadaApi from "../providers/useLlamadaApi";
+import { useEstetico } from '../providers/ProviderEstetico';
 
 export function Eventos() {
     const { datosApi, loading } = useLlamadaApi('events');
-
+    const { searchQuery } = useEstetico();
+    
     if (loading) {
         return <div>Loading...</div>;
     }
-
+    
+    // Filtrar eventos según el searchQuery 
+    const eventosFiltrados = datosApi.data.filter(evento =>
+        evento.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    
     return (
         <div className="eventos-main-container">
             <div className="eventos-container">
-                {datosApi.data.map((evento) => {
-                    console.log("Evento imagen:", evento.image);
-                    console.log("URL completa:", `https://yeray.informaticamajada.es/${evento.image}`);
-
-                    return (
-                        <Evento
-                            key={evento.id}
-                            id={evento.id}
-                            nombre={evento.title}
-                            bio={evento.description}
-                            imagen={evento.image}
-                            estado={evento.state}
-                            tipo={evento.type}
-                        />
-                    );
-                })}
+                {eventosFiltrados.map(evento => (
+                    <Evento
+                        key={evento.id}
+                        id={evento.id}
+                        nombre={evento.title}
+                        bio={evento.description}
+                        imagen={evento.image}
+                        estado={evento.state}
+                        tipo={evento.type}
+                    />
+                ))}
             </div>
-
             <VentanaDerecha />
         </div>
     );
+    
 }
 
 
